6fc1522eba97cb8c6aac1212a474f0b7295681dc
Robert Bubnik <rbubnik@gmail.com>
2024-04-02 22:32:18 -0400

    passed

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,15 @@
-# your code goes here!
+import time
+
+def countdown(integer):
+    while integer > 0:
+        print(f'{integer} SECOND(S)!')
+        integer -= 1
+    print('HAPPY NEW YEAR!')       
+
+def countdown_with_sleep(integer):
+    def delay(function):
+        time.sleep(3)
+    delay(countdown(integer))
+        
+
+    
